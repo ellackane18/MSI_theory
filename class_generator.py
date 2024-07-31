4fc51e3dbb55e6c6b1fec7e010312a1f12053ef7
Ella Kane <elk84@pitt.edu>
2024-07-31 11:17:30 -0400

    using ruff on one of lei's files. it looks good

--- class_generator.py
+++ class_generator.py
@@ -11,22 +11,30 @@ import Mess_class, os, copy, io
 
 # read in cleaned file and generate output classes
 # class_generator() is for regular MESS-input and class_generator_abstraction() is for abstraction reactions
-def class_generator(cleaned_file, keep_file = True):
+def class_generator(cleaned_file, keep_file=True):
     # define output classes
     results = {}
     save_flag = False
     cond_flag = False
     prev_species = None
     repeated_att = 0
-    curr_species = Mess_class.Computation_Cond()        # the first calss has to be computation conditions
+    curr_species = (
+        Mess_class.Computation_Cond()
+    )  # the first calss has to be computation conditions
     section_order = []
     files_to_copy = []
 
     # commands that need sepcical treatment (has unit and accross multiple lines)
-    spec_list = ['Geometry', 'Frequencies', 'ElectronicLevels', 'FragmentGeometry', 'FourierExpansion']
+    spec_list = [
+        "Geometry",
+        "Frequencies",
+        "ElectronicLevels",
+        "FragmentGeometry",
+        "FourierExpansion",
+    ]
 
     # read in the cleaned file
-    fhand = io.open(cleaned_file, 'rb')
+    fhand = io.open(cleaned_file, "rb")
     lines = fhand.readlines()
     fhand.close()
 
@@ -36,21 +44,25 @@ def class_generator(cleaned_file, keep_file = True):
 
     for line in lines:
         # get rid of space in the line
-        line = line.replace('\t', ' ')
-        key, value = line.split(' ')[0], line.split(' ')[1:]
-        value = [i.replace('\n', '').replace('\r', '').strip('[]\'\,') for i in value if i.strip()]
+        line = line.replace("\t", " ")
+        key, value = line.split(" ")[0], line.split(" ")[1:]
+        value = [
+            i.replace("\n", "").replace("\r", "").strip("[]'\,")
+            for i in value
+            if i.strip()
+        ]
 
         # determine the files to copy over
         for x in value:
-            if '.dat' in x or 'File' in key:
+            if ".dat" in x or "File" in key:
                 files_to_copy.append(x)
 
         # detremine if a unit is included in the line
         # com is for defining the molecular species class
         has_unit = False
         try:
-            com, unit = key.split('[')[0], key.split('[')[1]
-            unit = '[' + unit
+            com, unit = key.split("[")[0], key.split("[")[1]
+            unit = "[" + unit
             has_unit = True
         except IndexError:
             com = key
@@ -62,7 +74,11 @@ def class_generator(cleaned_file, keep_file = True):
             prev_species = copy.deepcopy(curr_species)
             curr_species = Mess_class.Collision_Relaxation()
         elif "Well" in key:
-            if not 'WellDepth' in key and not 'WellCutoff' in key and not 'WellExtension' in key:
+            if (
+                not "WellDepth" in key
+                and not "WellCutoff" in key
+                and not "WellExtension" in key
+            ):
                 save_flag = True
                 prev_species = copy.deepcopy(curr_species)
                 curr_species = Mess_class.Well()
@@ -81,11 +97,11 @@ def class_generator(cleaned_file, keep_file = True):
 
         elif com in curr_species.__dict__.keys():
             repeated_att += 1
-            com = str(com) + ' ' + str(repeated_att)
+            com = str(com) + " " + str(repeated_att)
 
         # parse the data from the input file
         # for energy, mass, and rotational constants (has unit but in the same line)
-        if has_unit and not com.split(' ')[0] in spec_list:
+        if has_unit and not com.split(" ")[0] in spec_list:
             if len(value) > 1:
                 value = [float(i) for i in value]
                 value = [str(value), unit]
@@ -93,40 +109,40 @@ def class_generator(cleaned_file, keep_file = True):
                 value.append(unit)
 
             curr_species.__dict__[com] = value
-            curr_species.__dict__['order'].append(com)
+            curr_species.__dict__["order"].append(com)
 
         # for geometry, frequencies, electroniclevels and hindered rotors (has unit and in multiple lines)
-        elif has_unit and com.split(' ')[0] in spec_list:
-            temp = {'unit' : unit}
-            if 'Geometry' in com:
+        elif has_unit and com.split(" ")[0] in spec_list:
+            temp = {"unit": unit}
+            if "Geometry" in com:
                 mole_pool = []
                 step = len(value) + 1
                 # the number 4 comes from 1 atom symbol + 3 Cartesian Coordinates
                 while step - 4 > 0:
                     mole = value.pop(0)
                     if len(mole_pool) == 0:
-                        mole = '1 ' + mole
+                        mole = "1 " + mole
                     else:
-                        mole = '%d ' %(int(mole_pool[-1].split(' ')[0]) + 1) + mole
+                        mole = "%d " % (int(mole_pool[-1].split(" ")[0]) + 1) + mole
                     mole_pool.append(mole)
                     temp_list = []
                     for y in xrange(3):
                         temp_list.append(float(value.pop(0)))
                     temp[mole] = temp_list
                     step -= 4
-                temp['order'] = mole_pool
-            elif 'Frequencies' in com:
+                temp["order"] = mole_pool
+            elif "Frequencies" in com:
                 value = [float(i) for i in value]
-                temp['value'] = value
-            elif 'ElectronicLevels' in com or 'FourierExpansion' in com:
+                temp["value"] = value
+            elif "ElectronicLevels" in com or "FourierExpansion" in com:
                 temp_list = []
                 for y in xrange(len(value) / 2):
                     y1 = float(value.pop(0))
                     y2 = float(value.pop(0))
-                    temp_list.append(tuple([y1,y2]))
-                temp['value'] = temp_list
+                    temp_list.append(tuple([y1, y2]))
+                temp["value"] = temp_list
             curr_species.__dict__[com] = temp
-            curr_species.__dict__['order'].append(com)
+            curr_species.__dict__["order"].append(com)
 
         else:
             try:
@@ -135,27 +151,27 @@ def class_generator(cleaned_file, keep_file = True):
                 value = value
 
             curr_species.__dict__[com] = value
-            curr_species.__dict__['order'].append(com)
+            curr_species.__dict__["order"].append(com)
 
         # save the classes into a dictionary
         if save_flag and cond_flag:
-            results['condition'] = prev_species
-            section_order.append('condition')
+            results["condition"] = prev_species
+            section_order.append("condition")
             save_flag = False
             cond_flag = False
-        elif save_flag and hasattr(prev_species, 'Model'):
-            results['col_rel'] = prev_species
-            section_order.append('col_rel')
+        elif save_flag and hasattr(prev_species, "Model"):
+            results["col_rel"] = prev_species
+            section_order.append("col_rel")
             save_flag = False
-        elif save_flag and hasattr(prev_species, 'Well'):
+        elif save_flag and hasattr(prev_species, "Well"):
             results[str(prev_species.Well[0])] = prev_species
             section_order.append(str(prev_species.Well[0]))
             save_flag = False
-        elif save_flag and hasattr(prev_species, 'Bimolecular'):
+        elif save_flag and hasattr(prev_species, "Bimolecular"):
             results[str(prev_species.Bimolecular[0])] = prev_species
             section_order.append(str(prev_species.Bimolecular[0]))
             save_flag = False
-        if save_flag and hasattr(prev_species, 'Barrier'):
+        if save_flag and hasattr(prev_species, "Barrier"):
             results[str(prev_species.Barrier[0])] = prev_species
             section_order.append(str(prev_species.Barrier[0]))
             save_flag = False
@@ -164,22 +180,31 @@ def class_generator(cleaned_file, keep_file = True):
     section_order.append(str(curr_species.Barrier[0]))
     return results, section_order, files_to_copy
 
-def class_generator_abstraction(cleaned_file, keep_file = False):
+
+def class_generator_abstraction(cleaned_file, keep_file=False):
     # define output classes
     results = {}
     save_flag = False
     cond_flag = False
-    prev_species = ''
+    prev_species = ""
     repeated_att = 0
-    curr_species = Mess_class.Computation_Cond()        # the first calss has to be computation conditions
+    curr_species = (
+        Mess_class.Computation_Cond()
+    )  # the first calss has to be computation conditions
     section_order = []
     files_to_copy = []
 
     # commands that need sepcical treatment (has unit and accross multiple lines)
-    spec_list = ['Geometry', 'Frequencies', 'ElectronicLevels', 'FragmentGeometry', 'FourierExpansion']
+    spec_list = [
+        "Geometry",
+        "Frequencies",
+        "ElectronicLevels",
+        "FragmentGeometry",
+        "FourierExpansion",
+    ]
 
     # read in the cleaned file
-    fhand = io.open(cleaned_file, 'rb')
+    fhand = io.open(cleaned_file, "rb")
     lines = fhand.readlines()
     fhand.close()
 
@@ -189,21 +214,25 @@ def class_generator_abstraction(cleaned_file, keep_file = False):
 
     for line in lines:
         # get rid of space in the line
-        line = line.replace('\t', ' ')
-        key, value = line.split(' ')[0], line.split(' ')[1:]
-        value = [i.replace('\n', '').replace('\r', '').strip('[]\'\,') for i in value if i.strip()]
+        line = line.replace("\t", " ")
+        key, value = line.split(" ")[0], line.split(" ")[1:]
+        value = [
+            i.replace("\n", "").replace("\r", "").strip("[]'\,")
+            for i in value
+            if i.strip()
+        ]
 
         # determine the files to copy over
         for x in value:
-            if '.dat' in x or 'File' in key:
+            if ".dat" in x or "File" in key:
                 files_to_copy.append(x)
 
         # detremine if a unit is included in the line
         # com is for defining the molecular species class
         has_unit = False
         try:
-            com, unit = key.split('[')[0], key.split('[')[1]
-            unit = '[' + unit
+            com, unit = key.split("[")[0], key.split("[")[1]
+            unit = "[" + unit
             has_unit = True
         except IndexError:
             com = key
@@ -226,11 +255,11 @@ def class_generator_abstraction(cleaned_file, keep_file = False):
 
         elif com in curr_species.__dict__.keys():
             repeated_att += 1
-            com = str(com) + ' ' + str(repeated_att)
+            com = str(com) + " " + str(repeated_att)
 
         # parse the data from the input file
         # for energy, mass, and rotational constants (has unit but in the same line)
-        if has_unit and not com.split(' ')[0] in spec_list:
+        if has_unit and not com.split(" ")[0] in spec_list:
             if len(value) > 1:
                 value = [float(i) for i in value]
                 value = [str(value), unit]
@@ -238,40 +267,40 @@ def class_generator_abstraction(cleaned_file, keep_file = False):
                 value.append(unit)
 
             curr_species.__dict__[com] = value
-            curr_species.__dict__['order'].append(com)
+            curr_species.__dict__["order"].append(com)
 
         # for geometry, frequencies, electroniclevels and hindered rotors (has unit and in multiple lines)
-        elif has_unit and com.split(' ')[0] in spec_list:
-            temp = {'unit' : unit}
-            if 'Geometry' in com:
+        elif has_unit and com.split(" ")[0] in spec_list:
+            temp = {"unit": unit}
+            if "Geometry" in com:
                 mole_pool = []
                 step = len(value) + 1
                 # the number 4 comes from 1 atom symbol + 3 Cartesian Coordinates
                 while step - 4 > 0:
                     mole = value.pop(0)
                     if len(mole_pool) == 0:
-                        mole = '1 ' + mole
+                        mole = "1 " + mole
                     else:
-                        mole = '%d ' %(int(mole_pool[-1].split(' ')[0]) + 1) + mole
+                        mole = "%d " % (int(mole_pool[-1].split(" ")[0]) + 1) + mole
                     mole_pool.append(mole)
                     temp_list = []
                     for y in xrange(3):
                         temp_list.append(float(value.pop(0)))
                     temp[mole] = temp_list
                     step -= 4
-                temp['order'] = mole_pool
-            elif 'Frequencies' in com:
+                temp["order"] = mole_pool
+            elif "Frequencies" in com:
                 value = [float(i) for i in value]
-                temp['value'] = value
-            elif 'ElectronicLevels' in com or 'FourierExpansion' in com:
+                temp["value"] = value
+            elif "ElectronicLevels" in com or "FourierExpansion" in com:
                 temp_list = []
                 for y in xrange(len(value) / 2):
                     y1 = float(value.pop(0))
                     y2 = float(value.pop(0))
-                    temp_list.append(tuple([y1,y2]))
-                temp['value'] = temp_list
+                    temp_list.append(tuple([y1, y2]))
+                temp["value"] = temp_list
             curr_species.__dict__[com] = temp
-            curr_species.__dict__['order'].append(com)
+            curr_species.__dict__["order"].append(com)
 
         else:
             try:
@@ -280,19 +309,19 @@ def class_generator_abstraction(cleaned_file, keep_file = False):
                 value = value
 
             curr_species.__dict__[com] = value
-            curr_species.__dict__['order'].append(com)
+            curr_species.__dict__["order"].append(com)
 
         # save the classes into a dictionary
         if save_flag and cond_flag:
-            results['condition'] = prev_species
-            section_order.append('condition')
+            results["condition"] = prev_species
+            section_order.append("condition")
             save_flag = False
             cond_flag = False
-        elif save_flag and hasattr(prev_species, 'Bimolecular'):
+        elif save_flag and hasattr(prev_species, "Bimolecular"):
             results[str(prev_species.Bimolecular[0])] = prev_species
             section_order.append(str(prev_species.Bimolecular[0]))
             save_flag = False
-        if save_flag and hasattr(prev_species, 'Barrier'):
+        if save_flag and hasattr(prev_species, "Barrier"):
             results[str(prev_species.Barrier[0])] = prev_species
             section_order.append(str(prev_species.Barrier[0]))
             save_flag = False
